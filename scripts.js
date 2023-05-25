88f206fec1d95bbcbd890a80b550df50f47e8c30
Anastase Tuyizere <42033331+TuyizeeAnastase@users.noreply.github.com>
2023-05-25 14:45:04 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function generate() {
+    const scriptInput = document.getElementById('movie_scripts');
+    const summary = document.getElementById('summary');
+    const characters = document.getElementById('characters');
+    const data = {
+        text: scriptInput.value
+      };
+    fetch('http://localhost:3500',{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        summary.textContent=JSON.stringify(data.summary)
+        characters.textContent=JSON.stringify(data.characters)
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+  }
